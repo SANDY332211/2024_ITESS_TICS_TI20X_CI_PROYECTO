172ae70e5cf97f0da6a876c26efa4740c433c619
Sandra <san2040513@gmail.com>
2024-05-24 13:32:56 -0500

    Inicio del Proyecto

---
+++ src/itess_tics_2024ci_portafolio/ITESS_TICS_2024CI_PORTAFOLIO.java
@@ -0,0 +1,17 @@
+/*
+ITESS TICS
+Enero-Junio
+Calculo integral portafolio
+ti 20x
+24/05/2024
+by JMP
+ */
+package itess_tics_2024ci_portafolio;
+
+public class ITESS_TICS_2024CI_PORTAFOLIO {
+
+    public static void main(String[] args) {
+        System.out.println("hola Fx");
+    }
+    
+}
